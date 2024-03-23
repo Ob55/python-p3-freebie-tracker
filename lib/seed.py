44afa2e6535085a4a9c6ee712599a40c450b3e73
Ob55 <brian55mwangi@gmail.com>
2024-03-23 14:11:57 +0300

    done

--- lib/seed.py
+++ lib/seed.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python3
 
 # Script goes here!
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Freebie(Base):
+    __tablename__ = 'freebies'
+
+    id = Column(Integer, primary_key=True)
+    dev_id = Column(Integer, ForeignKey('devs.id'))
+    company_id = Column(Integer, ForeignKey('companies.id'))
+    item_name = Column(String)
+    value = Column(Integer)
+
+    dev = relationship('Dev', backref='freebies')
+    company = relationship('Company', backref='freebies')
